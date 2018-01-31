97f646243cf432f4a3da56556af1342eaf2e0785
kPherox <admin@mail.kr-kp.com>
2018-02-01 03:54:47 +0900

    Add statusItem
    Add LSUIElement key to Info.plist

--- NowPlayingTweet/AppDelegate.swift
+++ NowPlayingTweet/AppDelegate.swift
@@ -11,16 +11,21 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var menu: NSMenu!
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+
+        self.statusItem.title = "NPT"
+        self.statusItem.highlightMode = true
+        self.statusItem.menu = self.menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
