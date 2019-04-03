b92132898bfa0960f093e65adba4c2d22c8eb7a2
kPherox <admin@mail.kr-kp.com>
2019-04-03 17:33:43 +0900

    Add client to account protocol

--- Sources/Social/Account.swift
+++ Sources/Social/Account.swift
@@ -10,7 +10,10 @@ import Foundation
 protocol Account {
     var provider: Provider { get }
 
+    var id: String { get }
     var name: String { get }
     var screenName: String { get }
     var avaterUrl: URL { get }
+
+    var client: Client? { get }
 }
