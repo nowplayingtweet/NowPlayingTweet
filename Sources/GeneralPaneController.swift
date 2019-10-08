6d99318cc9f5d78cf3f85f98e5cbc565de40397d
kPherox <admin@mail.kr-kp.com>
2019-10-08 15:29:49 +0900

    Refactoring GeneralPaneController
    - Use `NSGridView`
      - Change "Format Variables" list to Swift code instead of Interface Builder
    - Change to use `NSTextField` instead of `NSTextView` of textarea for post format
    - Remove dirty
      - Remove edit/change button
      - Remove text counter

--- Sources/GeneralPaneController.swift
+++ Sources/GeneralPaneController.swift
@@ -6,97 +6,60 @@
 **/
 
 import Cocoa
-import TwitterText
 
-class GeneralPaneController: NSViewController {
+class GeneralPaneController: NSViewController, NSTextFieldDelegate {
 
     static let shared: GeneralPaneController = {
         let windowController = NSStoryboard.main!.instantiateController(withIdentifier: .generalPaneController)
         return windowController as! GeneralPaneController
     }()
 
-    @IBOutlet weak var tweetFormatView: NSScrollView!
-    @IBOutlet var tweetFormat: NSTextView!
-    @IBOutlet weak var editButton: NSButton!
-    @IBOutlet weak var textCounter: NSTextField!
+    static let formatVariables: KeyValuePairs = [
+        "{{Title}}": "Track Title",
+        "{{Artist}}": "Artist Name",
+        "{{Album}}": "Album Title",
+        "{{AlbumArtist}}": "Album Artist Name",
+    ]
+
+    @IBOutlet weak var tweetFormat: NSTextField!
 
     private let userDefaults: UserDefaults = UserDefaults.standard
 
-    private var keyUpMonitor: Any?
-
-    private let twitterTextParser: TwitterTextParser = {
-        var twitterTextConfig = TwitterTextConfiguration(fromJSONResource: kTwitterTextParserConfigurationV2)
-        return TwitterTextParser(configuration: twitterTextConfig)
-    }()
+    @IBOutlet weak var gridView: NSGridView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        self.tweetFormat.string = (self.userDefaults.string(forKey: "TweetFormat"))!
-        self.updateCounter()
-    }
-
-    override func cancelOperation(_ sender: Any?) {
-        self.tweetFormat.string = (self.userDefaults.string(forKey: "TweetFormat"))!
-        self.completeEditing()
-    }
-
-    override func keyUp(with event: NSEvent) {
-        self.updateCounter()
-    }
-
-    @IBAction private func editFormat(_ sender: NSButton) {
-        if self.tweetFormat.isEditable {
-            self.change()
-        } else {
-            self.keyUpMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyUp, handler: { event -> NSEvent? in
-                self.keyUp(with: event)
-                return event
-            })
-
-            self.editButton.title = "Change"
-            self.editButton.keyEquivalent = "\r"
-            self.tweetFormatView.borderType = .bezelBorder
-            self.tweetFormat.textColor = .textColor
-            self.tweetFormat.drawsBackground = true
-            self.tweetFormat.isEditable = true
+        for (name, desc) in Self.formatVariables {
+            let nameLabel = NSTextField(labelWithString: name + ":")
+            nameLabel.alignment = .right
+            let descLabel = NSTextField(labelWithString: desc)
+            let variableRow = self.gridView.addRow(with: [nameLabel, descLabel])
+            variableRow.height = 17
+            variableRow.yPlacement = .top
         }
+
+        self.tweetFormat.stringValue = (self.userDefaults.string(forKey: "TweetFormat"))!
     }
 
     @IBAction private func resetFormat(_ sender: NSButton) {
         self.userDefaults.removeObject(forKey: "TweetFormat")
         self.userDefaults.synchronize()
-        self.tweetFormat.string = (self.userDefaults.string(forKey: "TweetFormat"))!
-        self.completeEditing()
+        self.tweetFormat.stringValue = (self.userDefaults.string(forKey: "TweetFormat"))!
     }
 
-    private func change() {
-        self.userDefaults.set(self.tweetFormat.string, forKey: "TweetFormat")
-        self.userDefaults.synchronize()
-        self.tweetFormat.string = (self.userDefaults.string(forKey: "TweetFormat"))!
-        self.completeEditing()
-    }
-
-    private func updateCounter() {
-        let results: TwitterTextParseResults = self.twitterTextParser.parseTweet(self.tweetFormat.string)
-        
-        self.textCounter.stringValue = String(results.weightedLength) + "/" + String(self.twitterTextParser.maxWeightedTweetLength())
-    }
-
-    private func completeEditing() {
-        self.updateCounter()
-
-        if self.keyUpMonitor != nil {
-            NSEvent.removeMonitor(self.keyUpMonitor!)
+    func controlTextDidChange(_ notification: Notification) {
+        guard let textField = notification.object as? NSTextField else {
+            return
         }
 
-        self.editButton.title = "Edit"
-        self.editButton.keyEquivalent = ""
-        self.tweetFormatView.borderType = .noBorder
-        self.tweetFormat.textColor = .labelColor
-        self.tweetFormat.drawsBackground = false
-        self.tweetFormat.isSelectable = false
+        self.userDefaults.set(textField.stringValue, forKey: "TweetFormat")
+    }
+
+    func controlTextDidEndEditing(_ notification: Notification) {
+        if let _ = notification.object as? NSTextField {
+            self.userDefaults.synchronize()
+        }
     }
 
 }
