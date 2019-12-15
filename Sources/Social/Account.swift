5a73383af900055f7c4dbb5da8cb973da4b46724
kPherox <admin@mail.kr-kp.com>
2019-12-15 18:10:34 +0900

    Add `isEqual` to Account protocol extension

--- Sources/Social/Account.swift
+++ Sources/Social/Account.swift
@@ -17,4 +17,16 @@ protocol Account {
     var avaterUrl: URL { get set }
 
     init(id: String, name: String, username: String, avaterUrl: URL)
+
+}
+
+extension Account {
+    func isEqual(_ account: Account?) -> Bool {
+        guard let account = account else {
+            return false
+        }
+
+        return type(of: self).provider == type(of: account).provider
+            && self.id == account.id
+    }
 }
