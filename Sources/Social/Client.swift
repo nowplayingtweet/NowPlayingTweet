13761880cc0722a7e41dd1949c0ee46212613223
kPherox <admin@mail.kr-kp.com>
2019-12-15 18:15:28 +0900

    Use `nil` for optional params in `Client` protocol

--- Sources/Social/Client.swift
+++ Sources/Social/Client.swift
@@ -8,17 +8,36 @@
 import Foundation
 
 protocol Client {
-    static func authorize(handler: @escaping (Client) -> Void)
+    typealias Success = () -> Void
+    typealias Failure = (Error) -> Void
+
+    static func authorize(handler: ((Credentials) -> Void)?, failure: Failure?)
 
     var credentials: Credentials { get }
 
-    init(credentials: Credentials)
+    init?(_: Credentials)
 
-    func revoke(handler: @escaping () -> Void)
+    func revoke(handler: Success?, failure: Failure?)
 
-    func verify(handler: @escaping (Account) -> Void)
+    func verify(handler: ((Account) -> Void)?, failure: Failure?)
 
-    func post(text: String, handler: @escaping () -> Void)
+    func post(text: String, image: Data?, handler: Success?, failure: Failure?)
+}
 
-    func post(text: String, image: Data, handler: @escaping () -> Void)
+extension Client {
+    static func authorize(handler: ((Credentials) -> Void)? = nil, failure: Failure? = nil) {
+        return Self.authorize(handler: handler, failure: failure)
+    }
+
+    func revoke(handler: Success? = nil, failure: Failure? = nil) {
+        return self.revoke(handler: handler, failure: failure)
+    }
+
+    func verify(handler: ((Account) -> Void)? = nil, failure: Failure? = nil) {
+        return self.verify(handler: handler, failure: failure)
+    }
+
+    func post(text: String, image: Data? = nil, handler: Success? = nil, failure: Failure? = nil) {
+        return self.post(text: text, image: image, handler: handler, failure: failure)
+    }
 }
